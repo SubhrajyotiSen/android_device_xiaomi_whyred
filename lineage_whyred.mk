73ec805762931cc33d10efe0a5c70faf4cdc16e2
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2019-04-07 07:52:59 +0000

    whyred: lineage-ify
    Change-Id: Ie23b09f539a98a23a3ccead074f1b6b6d27d0598

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -23,12 +23,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
 # Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
-PRODUCT_NAME := aosip_whyred
+PRODUCT_NAME := lineage_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
