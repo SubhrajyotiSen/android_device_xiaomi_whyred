c4129c120068859c86d042105940fac4972d7eea
Bladeyy <getsuga369@gmail.com>
2019-06-23 15:38:49 +0530

    whyred: prop: Force enable IMS/Radio props
     * Fixes ViLTE
    
    Change-Id: Id0b7f7fdc628983929fdb1f8a6919d024e567218

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -75,3 +75,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
+
+# Radio Options
+PRODUCT_PROPERTY_OVERRIDES += \
+   persist.vendor.radio.rat_on=combine \
+   persist.vendor.radio.data_ltd_sys_ind=1 \
+   persist.vendor.radio.data_con_rprt=1 \
+   persist.radio.calls.on.ims=1
